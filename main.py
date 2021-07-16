9f1a4f1bed80cf9d7284fa1b3973cdd16e1dc273
Kevin Andres Rosales Marquez <kevin.rosales98@hotmail.com>
2021-07-16 11:08:51 -0600

    Se encontro problema al querer correr las funciones de tras de otras se trabaja en el problema

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from functions import get_data_from_Clamudi, get_data_from_VivaAnuncios, get_data_from_Inmuebles24
+from functions import *
+"""
+    Existe un problema al llamar todas las funciones de golpe
+    se recomienda habar de una funcion en una funcion para la extraccion de datos
+    se esta trabajando en el problema
+
+
+
+    Para obtener informacion de Lamudi unicamente en main llamar la funcion
+    get_data_from_lamudi("Aqui va nombre de archivo")
+
+    Se hace lo mismo para los otros sitios.
+
+    Funciones activas por el momento:
+        get_data_from_lamudi()
+        get_data_from_VivaAnuncios()
+        get_data_from_Inmuebles24()
+"""
 
 
 if __name__ == '__main__':
-    #se habla a extractor de Lamudi
-    get_data_from_Clamudi("CLamudi")
-    #Se habla a extractor de Viva Anuncios
-    get_data_from_VivaAnuncios("CVivaAnuncios")
-
+    #get_data_from_Clamudi("lamudi")
+    #get_data_from_VivaAnuncios("ivaAnuncios")
     get_data_from_Inmuebles24("inmuebles24")
-
-
+    
